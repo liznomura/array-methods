68f82b5e5b1e13a9c1a8349125e98808cca35e86
liznomura <lizn@hawaii.edu>
2017-06-13 18:41:04 -1000

    finished pop();

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,7 +16,12 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+//push();
 var nameArray = [];
 nameArray.push('Liz', 'Nomura');
 
 genericNumberArray.push(6, 7, 8, 9, 10);
+
+//pop();
+var popResult = colors.pop();
