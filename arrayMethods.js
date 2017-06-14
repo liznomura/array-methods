76b2cf98b284f0997f25a729f8fa8c62f181c641
liznomura <lizn@hawaii.edu>
2017-06-13 20:11:17 -1000

    finished indexOf();

--- arrayMethods.js
+++ arrayMethods.js
@@ -64,3 +64,7 @@ var owesMoney = friends.slice(3,6);
 
 //toString();
 var monthNameString = monthNames.toString();
+
+//indexOf();
+var favoriteColor = colors.indexOf("Red");
+var favoriteEvenNumber = mixedNums.indexOf(76);
